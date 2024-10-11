c76e4008bbc4168c2faa94c907106dbce99b2e4e
Davi <vdrdavi@gmail.com>
2024-10-11 11:09:06 -0300

    Adicionando visão do jogador controlada pelo mouse

--- Assets/Scripts/MovimentacaoPersonagem.cs
+++ Assets/Scripts/MovimentacaoPersonagem.cs
@@ -17,5 +17,11 @@ public class MovimentacaoPersonagem : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(speedX, speedY).normalized * movSpeed;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(
+                    mousePosition.x - transform.position.x,
+                    mousePosition.y - transform.position.y
+                );
+        transform.up = direction;
     }
 }
